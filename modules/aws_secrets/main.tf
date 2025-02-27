f66096bf64b1233df43b92a63bd747b43098954a
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-02-27 17:21:14 +0000

    CCL-2254 moved to root

--- modules/aws_secrets/main.tf
+++ modules/aws_secrets/main.tf
@@ -9,7 +9,7 @@ module "secret_and_role" {
 
   for_each = var.aws_secrets
 
-  aws_secrets = tomap({"${each.key}"=each.value})
-  aws_account_id = data.aws_caller_identity.this_account.account_id
+  aws_secrets        = tomap({ "${each.key}" = each.value })
+  aws_account_id     = data.aws_caller_identity.this_account.account_id
   aws_account_region = data.aws_region.this_region.name
 }
